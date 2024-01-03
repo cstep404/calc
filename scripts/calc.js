5c7bd95d2fb61602cef2d870000c39ca78e65f99
Chip <cstep772@gmail.com>
2024-01-03 13:16:26 -0500

    Added initial functions and variables.

---
+++ scripts/calc.js
@@ -0,0 +1,18 @@
+// operation functions
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+// operator function - takes an operator (+, -, *, /) and two numbers (a and b)
+const operate = (a, operator, b) => a + operator + b;
+
+// variables
+let initialNumber;
+let operator = "";
+let nextNumber;
+
+
