de8f6627f1ccb98574986f73cf82e0dd603e0384
Chip <cstep772@gmail.com>
2024-01-07 13:46:56 -0500

    Added ability to calculate numbers in display.

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,10 +1,8 @@
 // SELECTORS
-
-// display
 const displaySelector = document.querySelector("#display");
 
 // use the buttons parent (the calculator div) to select the buttons using event delegation
-const btnSelector = document.querySelector("#calculator")
+const btnSelector = document.querySelector("#calculator");
 
 // operation functions
 const add = (a, b) => a + b;
@@ -16,12 +14,10 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 // variables
-let initialNumber;
-let operator;
-let nextNumber;
-
-// display
-let display;
+let initialNumber = "";
+let operator = "";
+let nextNumber = "";
+let bank = [];
 
 // operator function - takes an operator (+, -, *, /) and two numbers (a and b)
 // a is set to initialNumber var
@@ -52,22 +48,57 @@ const operate = (a, operator, b) => {
 
 // event listener - listens on each button
 btnSelector.addEventListener("click", (event) => {
+    console.log(event.target);
     if (event.target.tagName === "BUTTON") {
-        console.log(event.target.innerText);
-        displaySelector.textContent = event.target.innerText;
+        console.log("bank: ", bank);
+        if (event.target.classList[1] === "number" && bank.length === 0) {
+            // when a number is pressed it should be added to initialNumber and every number press after should also be added to initialNumber
+            initialNumber += event.target.textContent;
+            console.log(`initialNumber: ${initialNumber}`);
+            displaySelector.textContent = initialNumber;
+        }
+
+        if (event.target.classList[1] === "operator") {
+            // 1. push initialNumber string to the "bank"
+            bank.push(initialNumber);
+            console.log(`initialNumber: ${initialNumber}`);
+            console.log(bank);
+            // 2. set initialNumber to an empty string
+            initialNumber = "";
+            console.log(`initialNumber: ${initialNumber}`);
+            // 3. set operator to the chosen sign
+            operator += event.target.textContent;
+            console.log(`operator: ${operator}`);
+            displaySelector.textContent = operator;
+            // 4. push operator to "bank"
+            bank.push(operator);
+            console.log(bank);
+        }
+
+        if (event.target.classList[1] === "number" && bank.length > 0) {
+            nextNumber += event.target.textContent;
+            console.log(`nextNumber: ${nextNumber}`);
+            displaySelector.textContent = nextNumber;
+        }
+
+        // check size of array and if indexes already exist
+        // if array is equal to 3, fire operate function
+        if (bank.length === 2 && event.target.textContent === "=") {
+            // 1. push nextNumber string to the "bank"
+            bank.push(nextNumber);
+            console.log(`nextNumber: ${nextNumber}`);
+            console.log(bank);
+            // 2. set nextNumber to an empty string
+            nextNumber = "";
+            console.log(`nextNumber: ${nextNumber}`);
+            // 3. fire operator function to calculate. convert index 0 and index 2 from strings to numbers
+            console.log('firing operate function');
+            let answer = operate(Number(bank[0]), bank[1], Number(bank[2]));
+            // 4. set display to answer
+            displaySelector.textContent = answer;
+        }
     }
 });
-
-// solution var = stores final result of initialNumber, the operator, and next Number
-// ex. initialNumber = 2
-// operator = "+"
-// nextNumber = "8"
-// solution = 10
-
-let solution = operate(initialNumber, operator, nextNumber);
-
-// click a number and display should be updated to that number, then click an operator and display should be updated to the operator, finally click the last number and the display should update to that number while remembering the previous two operations
-// ex. click "2", display should show "2". "2" then gets stored in the "bank". "+" is then clicked, so "+" should be displayed. "+" is then stored in the "bank" along with "2". lastly, "8" is clicked which should be shown in the display. then when the user clicks "=" it should fire the "add" function
 
 // todo
 // 1. *** when result is returned, initialNumber needs to be set to the new result ***
